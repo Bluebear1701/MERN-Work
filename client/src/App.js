df4b27284835e4f8307be9a3fa9cb52b843189fe
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-06-26 18:26:42 -0500

    updating code

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<SearchBooks></SearchBooks>} />
+          <Route exact path='/saved' element={<SavedBooks></SavedBooks>} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Routes>
+      </>
+    </Router>
+  );
+}
+
+export default App;
