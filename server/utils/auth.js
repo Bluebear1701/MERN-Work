6afade8deb2f4f3303e56ad1bedbe7d9339c674a
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-07-03 16:29:45 -0500

    adding folders and updating auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,13 +6,16 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function ({req}) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+      .split(' ')
+      .pop()
+      .trim();
     }
 
     if (!token) {
@@ -26,10 +29,11 @@ module.exports = {
     } catch {
       console.log('Invalid token');
       return req
+      //return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-   return req
+    return req
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
